71512211acb4e4a2ed8c050cb4b8edc94dc91e40
Tang JiXing 唐继兴-IS <TangJiXing@saicmotor.com>
2018-11-19 13:44:13 +0800

    将解析sql的过程封装为API

--- sql2excel.py
+++ sql2excel.py
@@ -3,6 +3,7 @@ import re
 import xlwt
 import sys
 from xlwt import *
+import tkinter as tk
 
 
 class Entry(object):
@@ -106,132 +107,139 @@ class Styles(object):
             return self.goodstyle
         if style == "normal":
             return self.normalstyle
-        return NULL
+        return
+        
+class SQLParser(object):
+    """docstring for SQLParser"""
+    def __init__(self):
+        super(SQLParser, self).__init__()
 
+    def parse(self, sql, excelpath):
+        style = Styles()
+        book = xlwt.Workbook(encoding='utf-8',  style_compression=0)
+        table_match = "CREATE TABLE\s+`\S+`\s+[^;]*"
+        table_result = re.findall(table_match, sql)
+
+        table_name_array = []
+        table_name_sheet = {}
+        for x in range(0, len(table_result)):
+            table_content = table_result[x]
+            table_name = ""
+
+            tmp = re.findall(r'CREATE TABLE\s+`\S+`', table_content)
+            tmp = re.findall(r'`\S+`', tmp[0])
+            tmp = re.findall(r'[^`]+', tmp[0])
+            table_name = tmp[0]
+            if table_name[0:29] in table_name_sheet:
+                table_name_sheet[table_name[0:29]] += 1
+                table_name = table_name[0:29] + str(table_name_sheet[table_name[0:29]])
+            else:
+                table_name_sheet[table_name[0:29]] = 0
+                pass
+            table_name_array.append(table_name)
+
+            entrys = re.findall("[^\n]*", table_content)
+            entry_array = []
+            # print entrys
+            for y in range(0, len(entrys)):
+
+                tmp = re.findall("\S+", entrys[y])
+                if len(tmp) == 0:
+                    continue
+                tmp = re.findall("`\S+`", tmp[0])
+                if len(tmp) == 0:
+                    continue
+
+                # print entrys[y]
+                tmp = re.findall("`\S+`", entrys[y])
+                # names.append(re.findall("[^`]+",tmp[0])[0])
+                name = re.findall("[^`]+", tmp[0])[0]
+
+                # types.append(re.findall("\S+", entrys[y])[1])
+                typelength = re.findall("\S+", entrys[y])[1]
+
+                tmp = re.findall("(NOT NULL)|(DEFAULT\s+\S+)", entrys[y])
+                if len(tmp) != 0:
+                    # defaults.append(tmp[0])
+                    default = tmp[0]
+                    pass
+                else:
+                    default = "NULL"
+
+                tmp = re.findall("COMMENT\s+\'[^\']*\'", entrys[y])
+                if len(tmp) != 0:
+                    tmp = re.findall("\'[^\']*\'", tmp[0])
+                    tmp = re.findall("[^\'']+", tmp[0])
+                    # comments.append(tmp[0])
+                    comment = tmp[0]
+                    pass
+                else:
+                    comment = ""
+
+                entry_array.append(Entry(name, typelength, default, comment))
+
+                pass
+            if len(table_name) > 30:
+                table_name_tmp = table_name[0:30]
+            else:
+                table_name_tmp = table_name
+            print(table_name_tmp)
+            sheet = book.add_sheet(table_name_tmp.capitalize())
+            sheet.write(0, 0, "序号", style.getStyle("good"))
+            sheet.write(0, 1, "字段名", style.getStyle("good"))
+            sheet.write(0, 2, "中文描述", style.getStyle("good"))
+            sheet.write(0, 3, "类型（长度）", style.getStyle("good"))
+            sheet.write(0, 4, "约束", style.getStyle("good"))
+            sheet.write(0, 5, "是否敏感信息", style.getStyle("good"))
+            sheet.write(0, 6, "备注", style.getStyle("good"))
+
+            for i in range(0, len(entry_array)):
+                sheet.write(i + 1, 0, i + 1, style.getStyle("normal"))
+                sheet.write(
+                    i + 1, 1, entry_array[i].name.upper(), style.getStyle("normal"))
+                sheet.write(i + 1, 2, "", style.getStyle("normal"))
+                sheet.write(
+                    i + 1, 3, entry_array[i].typelength, style.getStyle("normal"))
+                sheet.write(
+                    i + 1, 4, entry_array[i].default, style.getStyle("normal"))
+                sheet.write(i + 1, 5, "N", style.getStyle("normal"))
+                sheet.write(
+                    i + 1, 6, entry_array[i].comment, style.getStyle("normal"))
+
+                pass
+
+        sheet = book.add_sheet("表清单")
+
+        sheet.write_merge(0, 0, 0, 6, "系统数据库表清单", style.getStyle("poor"))
+        sheet.write(1, 0, "系统名", style.getStyle("good"))
+        sheet.write(2, 0, "序号", style.getStyle("good"))
+
+        sheet.write_merge(1, 1, 1, 4, "无忧服务", style.getStyle("good"))
+        sheet.write(2, 1, "数据库名", style.getStyle("good"))
+        sheet.write(2, 2, "数据库中文名", style.getStyle("good"))
+        sheet.write(2, 3, "表名", style.getStyle("good"))
+        sheet.write(2, 4, "中文名", style.getStyle("good"))
+
+        sheet.write_merge(1, 1, 5, 6, "更新日期：", style.getStyle("good"))
+        sheet.write(2, 5, "是否存在敏感字段", style.getStyle("good"))
+        sheet.write(2, 6, "备注", style.getStyle("good"))
+
+        for x in range(0, len(table_name_array)):
+            sheet.write(x + 3, 0, x + 1, style.getStyle("normal"))
+            sheet.write(x + 3, 1, "", style.getStyle("normal"))
+            sheet.write(x + 3, 2, "", style.getStyle("normal"))
+            sheet.write(
+                x + 3, 3, table_name_array[x].upper(), style.getStyle("normal"))
+            sheet.write(x + 3, 4, "", style.getStyle("normal"))
+            sheet.write(x + 3, 5, "N", style.getStyle("normal"))
+            sheet.write(x + 3, 6, "", style.getStyle("normal"))
+        book.save(excelpath)
+
+        return
 
 if __name__ == "__main__":
 
-    style = Styles()
-
-    book = xlwt.Workbook(encoding='utf-8',  style_compression=0)
-
+    sqlparser = SQLParser()
     sql = open(sys.argv[1], "r", encoding='utf-8')
     sql_content = sql.read()
-
-    table_match = "CREATE TABLE\s+`\S+`\s+[^;]*"
-
-    table_result = re.findall(table_match, sql_content)
-    table_name_array = []
-    table_name_sheet = {}
-    for x in range(0, len(table_result)):
-        table_content = table_result[x]
-        table_name = ""
-
-        tmp = re.findall(r'CREATE TABLE\s+`\S+`', table_content)
-        tmp = re.findall(r'`\S+`', tmp[0])
-        tmp = re.findall(r'[^`]+', tmp[0])
-        table_name = tmp[0]
-        if table_name[0:29] in table_name_sheet:
-            table_name_sheet[table_name[0:29]] += 1
-            table_name = table_name[0:29] + str(table_name_sheet[table_name[0:29]])
-        else:
-            table_name_sheet[table_name[0:29]] = 0
-            pass
-        table_name_array.append(table_name)
-
-        entrys = re.findall("[^\n]*", table_content)
-        entry_array = []
-        # print entrys
-        for y in range(0, len(entrys)):
-
-            tmp = re.findall("\S+", entrys[y])
-            if len(tmp) == 0:
-                continue
-            tmp = re.findall("`\S+`", tmp[0])
-            if len(tmp) == 0:
-                continue
-
-            # print entrys[y]
-            tmp = re.findall("`\S+`", entrys[y])
-            # names.append(re.findall("[^`]+",tmp[0])[0])
-            name = re.findall("[^`]+", tmp[0])[0]
-
-            # types.append(re.findall("\S+", entrys[y])[1])
-            typelength = re.findall("\S+", entrys[y])[1]
-
-            tmp = re.findall("(NOT NULL)|(DEFAULT\s+\S+)", entrys[y])
-            if len(tmp) != 0:
-                # defaults.append(tmp[0])
-                default = tmp[0]
-                pass
-            else:
-                default = "NULL"
-
-            tmp = re.findall("COMMENT\s+\'[^\']*\'", entrys[y])
-            if len(tmp) != 0:
-                tmp = re.findall("\'[^\']*\'", tmp[0])
-                tmp = re.findall("[^\'']+", tmp[0])
-                # comments.append(tmp[0])
-                comment = tmp[0]
-                pass
-            else:
-                comment = ""
-
-            entry_array.append(Entry(name, typelength, default, comment))
-
-            pass
-        if len(table_name) > 30:
-            table_name_tmp = table_name[0:30]
-        else:
-            table_name_tmp = table_name
-        print(table_name_tmp)
-        sheet = book.add_sheet(table_name_tmp.capitalize())
-        sheet.write(0, 0, "序号", style.getStyle("good"))
-        sheet.write(0, 1, "字段名", style.getStyle("good"))
-        sheet.write(0, 2, "中文描述", style.getStyle("good"))
-        sheet.write(0, 3, "类型（长度）", style.getStyle("good"))
-        sheet.write(0, 4, "约束", style.getStyle("good"))
-        sheet.write(0, 5, "是否敏感信息", style.getStyle("good"))
-        sheet.write(0, 6, "备注", style.getStyle("good"))
-
-        for i in range(0, len(entry_array)):
-            sheet.write(i + 1, 0, i + 1, style.getStyle("normal"))
-            sheet.write(
-                i + 1, 1, entry_array[i].name.upper(), style.getStyle("normal"))
-            sheet.write(i + 1, 2, "", style.getStyle("normal"))
-            sheet.write(
-                i + 1, 3, entry_array[i].typelength, style.getStyle("normal"))
-            sheet.write(
-                i + 1, 4, entry_array[i].default, style.getStyle("normal"))
-            sheet.write(i + 1, 5, "N", style.getStyle("normal"))
-            sheet.write(
-                i + 1, 6, entry_array[i].comment, style.getStyle("normal"))
-
-            pass
-
-    sheet = book.add_sheet("表清单")
-
-    sheet.write_merge(0, 0, 0, 6, "系统数据库表清单", style.getStyle("poor"))
-    sheet.write(1, 0, "系统名", style.getStyle("good"))
-    sheet.write(2, 0, "序号", style.getStyle("good"))
-
-    sheet.write_merge(1, 1, 1, 4, "无忧服务", style.getStyle("good"))
-    sheet.write(2, 1, "数据库名", style.getStyle("good"))
-    sheet.write(2, 2, "数据库中文名", style.getStyle("good"))
-    sheet.write(2, 3, "表名", style.getStyle("good"))
-    sheet.write(2, 4, "中文名", style.getStyle("good"))
-
-    sheet.write_merge(1, 1, 5, 6, "更新日期：", style.getStyle("good"))
-    sheet.write(2, 5, "是否存在敏感字段", style.getStyle("good"))
-    sheet.write(2, 6, "备注", style.getStyle("good"))
-
-    for x in range(0, len(table_name_array)):
-        sheet.write(x + 3, 0, x + 1, style.getStyle("normal"))
-        sheet.write(x + 3, 1, "", style.getStyle("normal"))
-        sheet.write(x + 3, 2, "", style.getStyle("normal"))
-        sheet.write(
-            x + 3, 3, table_name_array[x].upper(), style.getStyle("normal"))
-        sheet.write(x + 3, 4, "", style.getStyle("normal"))
-        sheet.write(x + 3, 5, "N", style.getStyle("normal"))
-        sheet.write(x + 3, 6, "", style.getStyle("normal"))
-    book.save(sys.argv[1] + ".xls")
+    sqlparser.parse(sql_content, sys.argv[1]+".xls")
